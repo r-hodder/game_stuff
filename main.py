c893d67418e6dc01fc8623190e65f659c27172fe
Rikki Hodder <r.hodder@novafutur.com>
2020-11-08 23:08:49 +0000

    initial game structure

---
+++ main.py
@@ -0,0 +1,64 @@
+import random
+
+import pygame as pg
+
+from settings import (
+    BG_COLOUR,
+    FPS,
+    HEIGHT,
+    TITLE,
+    WIDTH
+)
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.run()
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        self.all_sprites.update()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+    def draw(self):
+        self.screen.fill(BG_COLOUR)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+
+g = Game()
+g.show_start_screen()
+
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
